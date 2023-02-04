496764de13dfcc0e945d21b75c405d35acfcb58e
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2023-02-05 01:17:52 +0400

    update user

--- src/api/user/type.ts
+++ src/api/user/type.ts
@@ -6,3 +6,14 @@ export interface IUser {
     delete(req: Request, res: Response): void
     update(req: Request, res: Response): void
 }
+
+
+export type LoginBody = {
+    firstName: string,
+    lastName?: string,
+    phone?: string,
+    email: string,
+    city?: string,
+    password: string,
+    img?: string,
+}
--- src/api/user/user.ts
+++ src/api/user/user.ts
@@ -1,5 +1,8 @@
+import { AppDataSource } from '../../data-source'
 import { Request, Response } from 'express'
-import { IUser } from './type'
+import { Users } from '../../entity/users'
+import { IUser, LoginBody } from './type'
+import bcrypt from 'bcrypt' 
 
 export class User implements IUser {
     constructor() {
@@ -10,8 +13,26 @@ export class User implements IUser {
 
     }
 
-    create(req: Request, res: Response) {
+    async create(req: Request, res: Response) {
+        
+        const { firstName, lastName, phone, email, city, password, img } = req.body as LoginBody
 
+        if (!firstName.trim() || !email.trim() || !password.trim()) return res.status(400).send({error: 'Invalid name or password or invalid email'})
+
+        const user = await AppDataSource.getRepository(Users).createQueryBuilder("users").where("users.email = :email", { email }).getOne()
+
+        if(user) return res.status(400).send({error: "User already exists"})
+
+        const hashPassword = await bcrypt.hash(password, 10)
+
+        await AppDataSource
+        .createQueryBuilder()
+        .insert()
+        .into(Users)
+        .values([{ firstName, lastName, phone, email, city, password: hashPassword, img }])
+        .execute()
+
+        return res.status(201).send({ message: 'User created successfully'})
     }
 
     delete(req: Request, res: Response) {
