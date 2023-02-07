14fcef67821e7201533ab75618c632940824f716
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2023-02-08 01:26:57 +0400

    product create

--- src/api/product/product.ts
+++ src/api/product/product.ts
@@ -1,6 +1,9 @@
 import { AppDataSource } from '../../data-source'
 import { Products } from '../../entity/product'
 import { Request, Response } from 'express'
+import { Photo } from '../../entity/photo'
+import ImageDataURI from 'image-data-uri'
+import { sign } from 'jsonwebtoken'
 import { IProduct } from "./type"
 
 export class Product implements IProduct {
@@ -16,8 +19,21 @@ export class Product implements IProduct {
 
     }
 
-    cretate(req: Request, res: Response){
+    cretate = async (req: Request, res: Response) => {
 
+        const {description, title, city, price , street, delivery, categoryId } = req.body
+
+        if(!description && !title && !city) return res.status(400).send({ error: 'Fill in all the fields'})
+
+        const insert = await AppDataSource.createQueryBuilder().insert().into(Products).values([{ description, title, city, price: price ?? null, street: street ?? null, delivery: delivery ?? null, category: categoryId, user: req.query.token[0]}]).execute()
+
+        for(let i = 0; i < req.body.images.length; i++){
+            const path = await sign({ userId: req.query.token[0]}, process.env.SECRET_JWT_ACCESS_KEY)
+            await AppDataSource.createQueryBuilder().insert().into(Photo).values([{ description: description ?? '', path: path + '.png', product: insert.raw.insertId}]).execute()
+            ImageDataURI.outputFile(req.body.images[i].dataURL, __dirname + `/../../../public/${path}.png`)
+        }
+
+        res.status(201).send({message: 'Product signed successfully'})
     }
 
     getProduct(req: Request, res: Response){
